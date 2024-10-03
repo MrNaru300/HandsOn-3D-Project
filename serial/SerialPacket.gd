02e09b1aa8f34dea0513965efebc3f757ab0ceba
MrNaru300 <bruno.santi.oli@gmail.com>
2024-10-03 13:58:58 +0200

    feat: SerialManager.gd

---
+++ serial/SerialPacket.gd
@@ -0,0 +1,11 @@
+extends Object
+
+class_name SerialPacket
+
+class PressureSensor:
+	var pressure : float
+	var id: int
+	
+	func parse(data: PackedByteArray) -> PressureSensor:
+		printerr("PressureSensor parse not implemented!")
+		return null
